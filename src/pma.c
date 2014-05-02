6defabcc785c0b53ac5e44f114f1a37d555bdcd0
Pablo Montes <pabmont@gmail.com>
2014-05-01 21:23:58 -0400

    First draft

--- src/pma.c
+++ src/pma.c
@@ -1,1 +1,71 @@
-#include <pma.h>
+#include "pma.h"
+
+struct pma {
+    uint64_t n;
+    uint64_t m;
+    key_t *array;
+};
+
+PMA pma_create (key_t *array, uint64_t n) {
+    PMA pma = (PMA)malloc (sizeof (pma_t));
+    pma->m = n;
+    pma->n = n;
+    pma->array = (key_t *)malloc (sizeof (key_t) * pma->m);
+    memset (pma->array, 0, sizeof (key_t) * pma->m);
+    return pma;
+}
+
+void pma_destroy (PMA *pma) {
+    if (*pma != NULL) {
+        if ((*pma)->array != NULL) {
+            free ((*pma)->array);
+            (*pma)->array = NULL;
+        }
+        free (*pma);
+        *pma = NULL;
+    }
+}
+
+bool pma_find (PMA pma, key_t key) {
+    uint64_t from = 0;
+    uint64_t to = pma->m;
+    while (from <= to) {
+        uint64_t mid = from + (to - from) / 2;
+        uint64_t left
+        while (pma_empty (pma, mid))
+            mid--;
+        if (pma->array [mid] == key)
+            return mid;
+        else if (pma->array [mid] < key)
+            from = mid + 1;
+        else
+            to = mid - 1;
+    }
+}
+
+bool pma_predecessor (PMA pma, key_t key) {
+}
+
+void pma_insert_after (PMA pma, uint64_t i, key_t key) {
+}
+
+void pma_insert (PMA pma, key_t key) {
+    uint64_t i = pma_predecessor (pma, key);
+    pma_insert_after (pma, i, key)
+}
+
+// Returns true if the array at position 0 is empty and false otherwise.
+// pre 0 <= i < pma_size (p)
+bool pma_empty (PMA p, uint64_t i) {
+    return p->array [i] == 0;
+}
+
+// Returns the number of positions currently in the array.
+uint64_t pma_size (PMA p) {
+    return p->m;
+}
+
+// Returns the number of elements currently in the array.
+uint64_t pma_count (PMA p) {
+    return p->n;
+}
--- src/pma.h
+++ src/pma.h
@@ -1,4 +1,33 @@
 #ifndef __PMA_H_
 #define __PMA_H_
 
-#endif
+// Questions for Michael:
+// How to compute the size of the array and the size of the segments?
+// How to compact things to the left
+// How to insert? Do we preprocess every element to find the position of the predecessor, or do we
+// do it on the fly?
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef uint64_t key_t;
+
+struct pma;
+typedef struct pma pma_t, *PMA;
+
+static const double t_0 = 0.75;
+static const double t_h = 1;
+static const double p_0 = 0.5;
+static const double p_h = 0.25;
+
+PMA pma_create (key_t *array, uint64_t n);
+void pma_destroy (PMA *pma);
+bool pma_find (PMA pma, key_t key);
+bool pma_predecessor (PMA pma, key_t key);
+void pma_insert_after (PMA pma, uint64_t i, key_t key);
+void pma_insert (PMA pma, key_t key);
+bool pma_empty (PMA p, uint64_t i);
+uint64_t pma_size (PMA p);
+uint64_t pma_count (PMA p);
+
+#endif  /* __PMA_H_ */
