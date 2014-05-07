c643433761bfc544becf439cb60c28436b407092
Pablo Montes <pabmont@gmail.com>
2014-05-07 13:26:13 -0400

    Added references

--- src/pma.h
+++ src/pma.h
@@ -14,6 +14,23 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+/*
+ * Packed-Memory Array as described in [1, 2, 3, 4].
+ *
+ * [1] Michael A. Bender, Erik D. Demaine, and Martin Farach-Colton.
+ *     Cache-oblivious B-trees. In Proceedings of the 41st Annual Symposium on
+ *     Foundations of Computer Science (FOCS 2000), pages 399–409, Redondo
+ *     Beach, California, USA, November 2000. IEEE Computer Society.
+ * [2] Michael A. Bender, Erik D. Demaine, and Martin Farach-Colton.
+ *     Cache-oblivious B-trees. SIAM Journal on Computing, 35(2):341–358, 2005.
+ * [3] Michael A. Bender and Haodong Hu. An adaptive packed-memory array. In
+ *     Proceedings of the 25th ACM SIGACT-SIGMOD-SIGART Symposium on Principles
+ *     of Database Systems (PODS 2006), pages 20–29, Chicago, Illinois, USA,
+ *     June 2006. ACM.
+ * [4] Michael A. Bender and Haodong Hu. An adaptive packed-memory array. ACM
+ *     Trans. Database Syst., 32(4), November 2007.
+ */
+
 #ifndef __PMA_H_
 #define __PMA_H_
 
