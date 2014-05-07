79f57f2366be08cb96914fc4a081a0e51ba34a96
Pablo Montes <pabmont@gmail.com>
2014-05-07 12:05:52 -0400

    ISC license

--- src/pma.h
+++ src/pma.h
@@ -1,19 +1,20 @@
+/*
+ * Copyright (c) 2014 Pablo Montes <pabmont@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifndef __PMA_H_
 #define __PMA_H_
-
-// Questions for Michael:
-// - How to compute the size of the array and the size of the segments?
-// - How to compact things to the left. When packing, are we allowed to use all used levels, or only
-// the one we are promoting to?
-// - Do we also promote elements that have been insterted in the meantime?
-// - How to insert? Do we preprocess every element to find the position of the predecessor, or do we
-// do it on the fly?
-// - Should we have an implicit tree even for the amortized version?
-// - Do we have to ``clean'' as we are packing?
-// - Is it true that there can be $O(n)$ cleaning processes active at the same time? Do we trigger a
-// step only on those windows that contain a new element being inserted? When do cleaning processes
-// stop? Is it something likne: when a cleaning process finishes, it kills all processes underneath
-// it? Can we do this in $o(n)$?
-
 
 #endif  /* __PMA_H_ */
